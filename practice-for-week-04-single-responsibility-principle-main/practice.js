605b379101ef8e5b6b047d6bd0aa9f2b1e8ee711
Khensu-Ra Love El <khensura.loveel@gmail.com>
2023-04-01 23:09:47 -0400

    SRP problem set completed

--- practice-for-week-04-single-responsibility-principle-main/practice.js
+++ practice-for-week-04-single-responsibility-principle-main/practice.js
@@ -1,4 +1,4 @@
-/******************************** CONSTANTS *********************************/ 
+/******************************** CONSTANTS *********************************/
 const applePieRecipe = [
   { name: "pie crust", cost: 10.00, quantity: 1 },
   { name: "sugar", cost: 3.00, quantity: 0.5 },
@@ -36,34 +36,46 @@ const recipes = {
 function bakeAndSellPies(pieType, pieQuantity, profitMargin) {
   // Find the recipe for the pieType specified
   const recipe = recipes[pieType];
-  // Bake the number of pies specified by the pieQuantity
-  for (let i = 0; i < pieQuantity; i++) {
-    // Print the ingredients for each ingredient in the recipe
-    let combiningMsg = `Combining ingredients for ${pieType}: `
-    combiningMsg += recipe.map(ingredient => ingredient.name).join(', ');
-    console.log(combiningMsg);
 
-    // Print the nth pie that was baked
-    console.log(`Baked pie ${i + 1}!`);
-  }
+  //Display each pie order
+  const displayOrder = makePieOrder;
+  displayOrder(recipe);
 
   // Print the cost of each pie based on the cost of each ingredient
-  const costOfPie = recipe.reduce((prev, current) => {
-    return prev + current.cost;
-  }, recipe[0].cost);
-  console.log(`Cost per pie: ${costOfPie}`);
+  const pricePerPie = pieCost();
 
   // Calculate the total cost of all the pies
-  const totalCost = costOfPie * pieQuantity;
+  const totalCost = pricePerPie * pieQuantity;
 
   // Print the total revenue calculated using the given profitMargin
   const revenue = totalCost * (profitMargin || 1.2);
   console.log(`Sold ${pieQuantity} pies for $${revenue.toFixed(2)}!`);
+
+  function pieCost() {
+    const costOfPie = recipe.reduce((prev, current) => {
+      return prev + current.cost;
+    }, recipe[0].cost);
+    console.log(`Cost per pie: ${costOfPie}`);
+    return costOfPie;
+  }
+
+  function makePieOrder(recipe) {
+    // Bake the number of pies specified by the pieQuantity
+    for (let i = 0; i < pieQuantity; i++) {
+      // Print the ingredients for each ingredient in the recipe
+      let combiningMsg = `Combining ingredients for ${pieType}: `;
+      combiningMsg += recipe.map(ingredient => ingredient.name).join(', ');
+      console.log(combiningMsg);
+
+      // Print the nth pie that was baked
+      console.log(`Baked pie ${i + 1}!`);
+    }
+  }
 }
 
 /******************************* LOCAL TESTS *******************************/
-// bakeAndSellPies("applePie", 5, 2.5);
-// bakeAndSellPies("pumpkinPie", 2);
+//bakeAndSellPies("applePie", 5, 2.5);
+bakeAndSellPies("pumpkinPie", 2);
 // bakeAndSellPies("cherryPie", 7, 1.7);
 
 /**************************************************************************/
