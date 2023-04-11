7fda7412156354683316fcb07d42c8c8272a1cc9
Khensu-Ra Love El <khensura.loveel@gmail.com>
2023-04-10 21:37:14 -0400

    finish inheritance project

--- practice-for-week-04-inheritance-main/classes/student.js
+++ practice-for-week-04-inheritance-main/classes/student.js
@@ -1,6 +1,28 @@
-const Person = require('./person');
+const Person = require("./person");
 
 // Your code here
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA) {
+    super(firstName, lastName);
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+  static compareGPA(firstStudent, secondStudent) {
+    const highestGPA =
+      firstStudent.GPA === secondStudent.GPA ?
+        "TIE" :
+        firstStudent.GPA > secondStudent.GPA ?
+          firstStudent :
+          secondStudent;
+
+    if (highestGPA === "TIE") {
+      return "Both students have the same GPA";
+    } else {
+      return `${highestGPA.firstName} ${highestGPA.lastName} has the higher GPA.`;
+    }
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- practice-for-week-04-inheritance-main/classes/teacher.js
+++ practice-for-week-04-inheritance-main/classes/teacher.js
@@ -1,6 +1,19 @@
 const Person = require('./person');
 
 // Your code here
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    let totalXP = 0;
+    teachers.forEach(teacher => totalXP += teacher.yearsOfExperience);
+    return totalXP;
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
